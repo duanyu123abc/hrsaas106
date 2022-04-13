52859f8103f2a5908adc53169267f46967748ff5
duanyu <1136177119@qq.com>
2022-04-13 17:49:28 +0800

    完成树形结构的显示

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,19 +1,91 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织架构
-      </h2>
+      <!-- 组织架构的内容- 头部-->
+      <el-card class="tree-card">
+        <!-- 放置结构内容 -->
+        <el-row type="flex" justify="space-between" align="middle" style="height: 40px">
+          <el-col>
+            <!-- 左侧内容 -->
+            <span>江苏传智播客教育科技股份有限公司</span>
+          </el-col>
+          <el-col :span="4">
+            <!-- 右侧内容 -->
+            <el-row type="flex" justify="end">
+              <el-col>负责人</el-col>
+              <el-col>
+                <!-- 放置下拉菜单 -->
+                <el-dropdown>
+                  <!-- 内容 -->
+                  <span>操作
+                    <i class="el-icon-arrow-down" />
+                  </span>
+                  <!-- 具名插槽 -->
+                  <el-dropdown-menu slot="dropdown">
+                    <!-- 下拉选项 -->
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+        <!-- 放置一个el-tree -->
+        <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
+          <!-- 传入内容 插槽内容 有多少节点就循环多少次 -->
+          <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据 data 每个节点的数据对象-->
+          <el-row slot-scope="{ data }" type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
+            <el-col>
+              <!-- 左侧内容 -->
+              <span>{{ data.name }}</span>
+            </el-col>
+            <el-col :span="4">
+              <!-- 右侧内容 -->
+              <el-row type="flex" justify="end">
+                <el-col>{{ data.manager }}</el-col>
+                <el-col>
+                  <!-- 放置下拉菜单 -->
+                  <el-dropdown>
+                    <!-- 内容 -->
+                    <span>操作
+                      <i class="el-icon-arrow-down" />
+                    </span>
+                    <!-- 具名插槽 -->
+                    <el-dropdown-menu slot="dropdown">
+                      <!-- 下拉选项 -->
+                      <el-dropdown-item>添加子部门</el-dropdown-item>
+                      <el-dropdown-item>编辑部门</el-dropdown-item>
+                      <el-dropdown-item>删除子部门</el-dropdown-item>
+                    </el-dropdown-menu>
+                  </el-dropdown>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      departs: [{ name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹chong' }] },
+        { name: '行政部', manager: 'liubei' }, { name: '人事部', manager: 'sunquan' }],
+      defaultProps: {
+        label: 'name' // 表示从这个属性显示内容
+        // children: 'children' // 从这个属性找子节点
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.tree-card {
+  padding: 30px  140px;
+  font-size:14px;
+}
 </style>
