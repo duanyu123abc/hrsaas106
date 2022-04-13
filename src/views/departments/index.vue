43afa68c6af4a5ed9eecd2f6d0d51352151fb073
duanyu <1136177119@qq.com>
2022-04-13 22:41:57 +0800

    将树形内容单独抽提组件

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -4,64 +4,12 @@
       <!-- 组织架构的内容- 头部-->
       <el-card class="tree-card">
         <!-- 放置结构内容 -->
-        <el-row type="flex" justify="space-between" align="middle" style="height: 40px">
-          <el-col>
-            <!-- 左侧内容 -->
-            <span>江苏传智播客教育科技股份有限公司</span>
-          </el-col>
-          <el-col :span="4">
-            <!-- 右侧内容 -->
-            <el-row type="flex" justify="end">
-              <el-col>负责人</el-col>
-              <el-col>
-                <!-- 放置下拉菜单 -->
-                <el-dropdown>
-                  <!-- 内容 -->
-                  <span>操作
-                    <i class="el-icon-arrow-down" />
-                  </span>
-                  <!-- 具名插槽 -->
-                  <el-dropdown-menu slot="dropdown">
-                    <!-- 下拉选项 -->
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-          </el-col>
-        </el-row>
+        <tree-tools :tree-node="company" :is-root="true" />
         <!-- 放置一个el-tree -->
         <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
           <!-- 传入内容 插槽内容 有多少节点就循环多少次 -->
           <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据 data 每个节点的数据对象-->
-          <el-row slot-scope="{ data }" type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
-            <el-col>
-              <!-- 左侧内容 -->
-              <span>{{ data.name }}</span>
-            </el-col>
-            <el-col :span="4">
-              <!-- 右侧内容 -->
-              <el-row type="flex" justify="end">
-                <el-col>{{ data.manager }}</el-col>
-                <el-col>
-                  <!-- 放置下拉菜单 -->
-                  <el-dropdown>
-                    <!-- 内容 -->
-                    <span>操作
-                      <i class="el-icon-arrow-down" />
-                    </span>
-                    <!-- 具名插槽 -->
-                    <el-dropdown-menu slot="dropdown">
-                      <!-- 下拉选项 -->
-                      <el-dropdown-item>添加子部门</el-dropdown-item>
-                      <el-dropdown-item>编辑部门</el-dropdown-item>
-                      <el-dropdown-item>删除子部门</el-dropdown-item>
-                    </el-dropdown-menu>
-                  </el-dropdown>
-                </el-col>
-              </el-row>
-            </el-col>
-          </el-row>
+          <tree-tools slot-scope="{ data }" :tree-node="data" />
         </el-tree>
       </el-card>
     </div>
@@ -69,9 +17,14 @@
 </template>
 
 <script>
+import TreeTools from './components/tree-tools.vue'
 export default {
+  components: {
+    TreeTools
+  },
   data() {
     return {
+      company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' }, // 就是头部的数据结构
       departs: [{ name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹chong' }] },
         { name: '行政部', manager: 'liubei' }, { name: '人事部', manager: 'sunquan' }],
       defaultProps: {
