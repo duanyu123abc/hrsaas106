bc0f3a452aae46f98919237891e7096f30c9e7b5
duanyu <1136177119@qq.com>
2022-04-26 22:02:29 +0800

    封装一个通用的工具栏

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import '@/styles/index.scss' // global css
 import App from './App'
 import store from './store'
 import router from './router'
-
+import Components from '@/components'
 import * as directives from '@/directives'
 import '@/icons' // icon
 import '@/permission' // permission control
@@ -24,6 +24,8 @@ Vue.use(ElementUI, { locale })
 Object.keys(directives).forEach(key => {
   Vue.directive(key, directives[key]) // 注册自定义指令
 })
+// 注册自定义组件
+Vue.use(Components)
 Vue.config.productionTip = false
 
 // vue实例化
