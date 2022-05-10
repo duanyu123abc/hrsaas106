bcca8bcd852cf5277447d97ca17dc2d298c8b6e0
duanyu <1136177119@qq.com>
2022-05-10 16:20:23 +0800

    实现页面的全屏功能

---
+++ src/components/ScreenFull/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <svg-icon icon-class="fullscreen" style="color:#fff; width: 20px; height: 20px" @click="changeScreen" />
+  </div>
+</template>
+
+<script>
+// import ScreenFull from 'screenfull'
+export default {
+  methods: {
+    changeScreen() {
+      // 原生方式
+      document.documentElement.requestFullscreen()
+    //   if (!ScreenFull.isEnabled) {
+    //     // 此时全屏不可用
+    //     this.$message.warning('此时全屏组件不可用')
+    //     return
+    //   }
+      //   如果可用 就可以全屏
+    //   ScreenFull.toggle()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ import PageTools from './PageTools'
 import UploadExcel from './UploadExcel'
 import ImageUpload from './ImageUpload'
 import Print from 'vue-print-nb'
+import ScreenFull from './ScreenFull'
 export default {
   install(Vue) {
     // 组件的注册
@@ -10,6 +11,7 @@ export default {
     Vue.component('UploadExcel', UploadExcel) // 注册导入excel组件
     Vue.component('ImageUpload', ImageUpload) // 注册图片上传组件
     Vue.use(Print) // 注册打印组件
+    Vue.component('ScreenFull', ScreenFull)
   }
 }
 // vue.use()
